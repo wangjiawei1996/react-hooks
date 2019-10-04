0e68069f7e4fa193973c31b13bb6c2e2321c3a97
王嘉巍 <906789088@qq.com>
2019-10-04 12:34:31 +0800

    componentDidmount and componentUpdate in react to finish useEffect

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,7 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect} from 'react'
 const LikeButton = () => {
   const [like, setLike] = useState(0)
   const [ on, setOn] = useState(true)
+  useEffect(() => {
+    document.title = `点击了${like}次 `
+  })
   return (
     <div>
       <button onClick={() => {setLike(like + 1)}}>
