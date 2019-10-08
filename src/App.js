851abc3d887935b447e981f609433101e7903368
王嘉巍 <906789088@qq.com>
2019-10-08 19:44:03 +0800

    clear useEffect in MouseTracker and return this function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LikeButton from './components/LikeButton'
+import MouseTracker from './components/MouseTracker'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MouseTracker />
         <LikeButton />
         <a
           className="App-link"
